69f0bf1513a0e5b2129ef617c180c766bea1e50a
Redeemer Moses <redeemermoses05@gmail.com>
2025-08-11 14:33:03 +0100

    Update dbController.js

--- controller/dbController.js
+++ controller/dbController.js
@@ -1,21 +1,17 @@
-// controller/dbController.js
 const mongoose = require("mongoose");
-require("dotenv").config(); // loads the DB string from .env file
 
-const dbConnectionString = process.env.DBSTRING;
+require("dotenv").config();
 
-const connectDB = async () => {
-  try {
-    console.log("Connecting to DB...");
-    await mongoose.connect(dbConnectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("✅ Connected to MongoDB!");
-  } catch (error) {
-    console.error("❌ Error connecting to DB:", error.message);
-    process.exit(1); // Optional: Exit if unable to connect
-  }
+const dbConnectionString= process.env.DBSTRING;
+
+const connectDB= async () => {
+    try{
+        console.log("connecting to db...")
+        await mongoose.connect(dbConnectionString, {});
+        console.log("connection to db established 100 ✅");
+        }catch (error){
+            console.error("Error connecting to db:", error );
+        }
 };
 
 module.exports = connectDB;
